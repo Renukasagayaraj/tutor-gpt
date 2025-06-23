81f9f0513a2d0b4233d98ed7f77e71c418a04048
renuka_gbrits <renuka.sagayaraj@gbritsolutions.com>
2025-06-23 20:40:02 +0530

    log thevalues

--- utils/honcho.ts
+++ utils/honcho.ts
@@ -5,21 +5,47 @@ export const honcho = new Honcho({
   baseURL: process.env.HONCHO_URL!,
 });
 
+// export const getHonchoApp = unstable_cache(
+//   async () => {
+//     return await honcho.apps.getOrCreate(process.env.HONCHO_APP_NAME!, {
+//       timeout: 5 * 1000,
+//       maxRetries: 5,
+//     });
+//   },
+//   [],
+//   {
+//     revalidate: 300, // 5 minutes
+//   }
+// );
+
 export const getHonchoApp = unstable_cache(
   async () => {
-    return await honcho.apps.getOrCreate(process.env.HONCHO_APP_NAME!, {
-      timeout: 5 * 1000,
-      maxRetries: 5,
-    });
+    try {
+      console.log('🔄 Fetching Honcho app...');
+      console.log("honcho", honcho);
+      
+      console.log('Honcho app name:', process.env.HONCHO_APP_NAME);
+      return await honcho.apps.getOrCreate(process.env.HONCHO_APP_NAME!, {
+        timeout: 5 * 1000,
+        maxRetries: 5,
+      });
+    } catch (err) {
+      console.error('❌ Error in getHonchoApp:', err);
+      throw err;
+    }
   },
   [],
   {
-    revalidate: 300, // 5 minutes
+    revalidate: 300,
   }
 );
 
+
 export const getHonchoUser = unstable_cache(
   async (userId: string) => {
+    console.log('🔄 Fetching Honcho user...');
+    console.log('Honcho app name:', process.env.HONCHO_APP_NAME);
+    console.log('userId:', userId);
     const app = await getHonchoApp();
     return await honcho.apps.users.getOrCreate(app.id, userId, {
       timeout: 5 * 1000,
