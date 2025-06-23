2302ef4e4331b74353cde995054d70f305279a84
renuka_gbrits <renuka.sagayaraj@gbritsolutions.com>
2025-06-23 18:50:28 +0530

    remove the log

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -2,10 +2,7 @@ import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
 export async function createClient() {
-  // const cookieStore = await cookies();
-  const cookieStore = cookies(); // ✅ correct
-
-  console.log('[createClient] Getting cookies:', cookieStore.getAll());
+  const cookieStore = await cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -13,28 +10,18 @@ export async function createClient() {
     {
       cookies: {
         getAll() {
-          const allCookies = cookieStore.getAll();
-          console.log('[Supabase getAll cookies]', allCookies);
-          return allCookies;
-          // return cookieStore.getAll();
+          return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
           try {
-            console.log('[Supabase setAll cookies]', cookiesToSet);
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             );
-          } catch (err) {
-            console.warn(
-              '[Supabase setAll error] Likely called in Server Component:',
-              err
-            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
           }
-          // catch {
-          //   // The `setAll` method was called from a Server Component.
-          //   // This can be ignored if you have middleware refreshing
-          //   // user sessions.
-          // }
         },
       },
     }
